99d13798efaa6cd7578eb9c0e2149402bb1a692b
CallMeMichal <michaltulej1@gmail.com>
2023-03-21 15:13:47 +0100

    xx

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 package src;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("gello");
+        ggg sss = new ggg();
+
+        SwingUtilities.invokeLater(sss);
     }
 }
 
---
+++ src/ggg.java
@@ -0,0 +1,15 @@
+package src;
+
+import javax.swing.*;
+
+public class ggg implements Runnable {
+    private JButton PLNDODANEJWALUTYButton;
+    private JButton wyswietlanieKonkretnegoKursuWalutyButton;
+    private JButton aktualnaPogodaWMiescieButton;
+    private JButton STRONAWIKIPEDIIDLADANEGOButton;
+
+    @Override
+    public void run() {
+
+    }
+}
